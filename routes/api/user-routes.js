881e630132f39f85238793bf9965110ca89baaef
Sam Knopper <samanthaknopper@hotmail.com>
2022-06-26 22:07:00 +0800

    user delete route working, create , find all and find one

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const User = require('../../models/user');
+const Thought = require('../../models/thoughts')
 
 const router = require('express').Router();
 
@@ -16,13 +17,12 @@ router.get('/api/users', (req,res) => {
 
 router.get('/api/users/:id', (req,res) =>{
     //get user by id
-    User.findOne({_id: req.params.user_id})
+    User.findOne({_id: req.params.id})
     .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
-          : res.json({
-              student
-            }))
+          : res.json({user})
+        )
 });
 
 router.post('/api/users', (req,res) => {
@@ -34,11 +34,30 @@ router.post('/api/users', (req,res) => {
 
 router.put('/api/users/:id', (req,res) =>{
     //update a user
+
 });
 
 router.delete('/api/users/:id', (req,res) =>{
     //delete a user
-    User.findOne({ _id: req.params.userId })
+    User.findOneAndRemove({ _id: req.params.id })
+        .then((user) => 
+            !user
+             ? res.status(404).json({message: 'No user exists by the ID'})
+             : Thought.findOneAndUpdate(
+                {users: req.params.id},
+                {$pull: {users: req.params.id}},
+                {new:true}
+             )
+        )
+        .then((thought) => 
+          !thought
+           ? res.status(404).json({ message: 'User deleted, but no thoughts found',}) 
+           : res.json({ message: 'User successfully deleted'})
+           )
+           .catch((err) => {
+            console.log(err);
+            res.status(500).json(err, {message:"whoops caught an err"});
+           })
 });
 
 module.exports = router;
