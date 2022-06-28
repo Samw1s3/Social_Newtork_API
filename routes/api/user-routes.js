5545da57da193a7d08404809b81c4af73600e5c8
Sam Knopper <samanthaknopper@hotmail.com>
2022-06-28 17:21:44 +0800

    user routes working again

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,7 +4,7 @@ const Thought = require('../../models/thoughts')
 const router = require('express').Router();
 
 
-router.get('/api/users', (req,res) => {
+router.get('/', (req,res) => {
     //get all of the users
     User.find({})
      .populate('thoughts')
@@ -15,7 +15,7 @@ router.get('/api/users', (req,res) => {
 
 });
 
-router.get('/api/users/:id', (req,res) =>{
+router.get('/:id', (req,res) =>{
     //get user by id
     User.findOne({_id: req.params.id})
     .then((user) =>
@@ -25,20 +25,20 @@ router.get('/api/users/:id', (req,res) =>{
         )
 });
 
-router.post('/api/users', (req,res) => {
+router.post('/', (req,res) => {
     //create a user
     User.create(req.body)
     .then((user) => res.json(user))
     .catch((err) => res.status(500).json(err));
 });
 
-router.put('/api/users/:id', (req,res) =>{
+router.put('/:id', (req,res) =>{
     //update a user
     User.findOneAndUpdate({_id: req.params.id})
 
 });
 
-router.delete('/api/users/:id', (req,res) =>{
+router.delete('/:id', (req,res) =>{
     //delete a user
     User.findOneAndRemove({ _id: req.params.id })
         .then((user) => 
