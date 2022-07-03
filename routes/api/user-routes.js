c8072ba702227a32e39cb7c339dceaa2617a3286
Sam Knopper <samanthaknopper@hotmail.com>
2022-07-03 21:00:35 +0800

    add catch

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -25,6 +25,7 @@ router.get('/:id', (req,res) =>{
           ? res.status(404).json({ message: 'No user with that ID' })
           : res.json({user})
         )
+    .catch((err) => res.status(500).json(err));
 });
 
 //create a user
